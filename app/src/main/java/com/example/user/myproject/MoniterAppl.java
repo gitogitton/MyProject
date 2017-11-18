551895cf87552427ca72bb61ade23b6894ab7258
gitogitton <mukyu_02@yahoo.co.jp>
2017-11-19 08:32:04 +0900

    Initial commit

---
+++ app/src/main/java/com/example/user/myproject/MoniterAppl.java
@@ -0,0 +1,19 @@
+package com.example.user.myproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoniterAppl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moniter_appl);
+
+        //toolbar setting
+
+        //show list
+
+
+    }
+}
